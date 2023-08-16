dc799eb55c5989a9282afb6951432e2a9d3c2817
@eundeom <121565744+eundeom@users.noreply.github.com>
2023-08-16 00:26:36 -0700

    Time: 417 ms (95.95%), Space: 29 MB (59.63%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def containsDuplicate(self, nums):
+        # set(집합)은 중복을 허용하지 않는다.
+        # set과 array의 길이가 다르면 중복이 있다는 뜻 (True)
+        
         num_set = set(nums)
         
         if len(num_set) != len(nums):

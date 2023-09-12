c77792f35ac0b1f6daae53005f42e48f17e8814d
@eundeom <121565744+eundeom@users.noreply.github.com>
2023-09-12 14:18:59 -0700

    Time: 2077 ms (5.18%), Space: 14.4 MB (84.46%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        length = len(nums) + 1
+        # arr = list(range(0,length))
+        arr = [0] * length
+        
+        for i in range(0,length):
+            if i in nums:
+                arr[i] = 1
+            
+        for i in range(0,length):
+            if arr[i] == 0:
+                return i
+            
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # len 만큼 배열안에 숫자가 다 있는지 check 
+        
+        # list 0으로 된 거 만들고 숫자가 있으면 index의 값을 1로 변경 
+        # for문 돌려서 0이 있는지 확인 - 출력.
